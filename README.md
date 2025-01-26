91a658e68190d82423ae57224e487e754cfb533e
jjoonior <jjoonior97@gmail.com>
2025-01-26 21:24:03 +0900

    k8s 연결 방식 변경 .kube/config -> token

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # springboot-k8s-playground
 
 - 쿠버네티스 클러스터의 `~/.kube/config` 파일로 쿠버네티스 연결
-  - 홈서버에 설치된 microk8s 기준 
-    - 노드 상관없이 `microk8s config` 명령어 출력을 스프링 서버의 `~/.kube/config` 로 저장
+  - 홈서버에 설치된 microk8s 기준
+  - 노드 상관없이 `microk8s config` 명령어 출력을 스프링 서버의 `~/.kube/config` 로 저장
 - `fabric8`
   - `~/.kube/config` 기본 설정 경로를 이용할 경우 config 설정 간편
   - 공식 `kubernetes-client` 보다 상대적으로 더 무겁지만 훨씬 간단하고 자연스러운 API 제공
+- k8s 연결 시 `~/.kube/config` 방식에서 토큰 방식으로 변경
+  - 외부에서 접근 가능하도록 microk8s 설정하고 config 파일에 포트포워딩 ip, port로 설정했으나 연결 안됨
+  - `MasterUrl`: 포트포워딩 ip, port
+  - `OauthToken`: 노드에서 기본 계정(SA) 토큰 확인
+    - `kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep default-token | awk '{print $1}')`
+  - `TrustCerts`: true
--- src/main/java/site/pading/demo/config/KubernetesConfig.java
+++ src/main/java/site/pading/demo/config/KubernetesConfig.java
@@ -1,16 +1,34 @@
 package site.pading.demo.config;
 
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KubernetesConfig {
+
+  @Value("${kubernetes.master-url}")
+  private String masterUrl;
+
+  @Value("${kubernetes.token}")
+  private String token;
+
+  @Value("${kubernetes.trust-cert}")
+  private boolean trustCert;
+
   @Bean
-  public KubernetesClient kubernetesClient() {
-    // 기본적으로 .kube/config 파일을 자동으로 읽음
-    return new DefaultKubernetesClient();
+  public KubernetesClient createClient() {
+    Config config = new ConfigBuilder()
+        .withMasterUrl(masterUrl)
+        .withOauthToken(token)
+        .withTrustCerts(trustCert)
+        .build();
+
+    return new DefaultKubernetesClient(config);
   }
 }
 
